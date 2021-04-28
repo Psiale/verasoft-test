011fd4d94efe1be3bded6b701cff2c76617f7fe9
Alexis Sanchez <psialedev@gmail.com>
2021-04-28 01:25:00 -0500

    Finished UserInfoContainer by adding the CommunicationActivities container to it

---
+++ src/components/CommunicationActivity.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const CommunicationActivity = ({activity, activityText}) => (
+    <div>
+        <p>{activity}</p>
+        <p>{activityText}</p>
+    </div>
+);
+
+export default CommunicationActivity;
